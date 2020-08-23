46d6c2cb51bb994216f03c5e3a3c4d369b2d8bbf
POMIN-163 <1637766030@qq.com>
2020-08-23 13:20:56 +0800

    2020-8-23

--- Class and object/Class and object/Program.cs
+++ Class and object/Class and object/Program.cs
@@ -22,25 +22,27 @@ namespace Class_and_object
             b.ShowDialog();
             Form c = b;
             // 实例调用(另起名字), 并非克隆！！,b、c 指向的是一个实例操作实例的属性会导致覆盖！！
-            c.Text = "POMIN-3";
+            c.Text = "POMIN-3";// 属性覆盖！！
             b.ShowDialog();
             Console.WriteLine("OK");
 
 
             Pomin pomin = new Pomin();// 调用类的方法前最好要先 new 一个实例
+            Console.Write("19 + 15 = "); 
             pomin.AddWrite(19, 15);
+            Console.Write("\n19 - 15 = ");
             pomin.SubWrite(19, 15);
-            Console.WriteLine(pomin.FacWrite_1(10));
-            Console.WriteLine(pomin.FacWrite_2(10));
-            Console.WriteLine(pomin.Sum_1(1, 100));
-            Console.WriteLine(pomin.Sum_2(1, 100));
-            Console.WriteLine(pomin.汉诺塔(3, "柱1", "柱2", "柱3"));
+            Console.WriteLine("\n10的阶乘(循环)：" + pomin.FacWrite_1(10));
+            Console.WriteLine("10的阶乘(递归)：" + pomin.FacWrite_2(10));
+            Console.WriteLine("1到100的和(循环)：" + pomin.Sum_1(1, 100));
+            Console.WriteLine("1到100的和(递归)：" + pomin.Sum_2(1, 100));
+            Console.WriteLine("3个环共计需搬运(汉诺塔问题 - 递归)：" + pomin.Hanoi(3) + "次");
         }
     }
     class Pomin
     {
-        public void AddWrite(int a, int b) => Console.WriteLine(a + b);
-        public void SubWrite(int a, int b) => Console.WriteLine(a - b);
+        public void AddWrite(int a, int b) => Console.Write(a + b);
+        public void SubWrite(int a, int b) => Console.Write(a - b);
         public int FacWrite_1(int x)// 循环阶乘
         {
             int result = 1;
@@ -83,17 +85,17 @@ namespace Class_and_object
                 return max;
             }
         }
-        public int 汉诺塔(int 数量, string 柱子1, string 柱子2, string 柱子3)
-        {// 柱子1 -> 柱子3           
-            if (数量 == 1)
+        public int Hanoi(int num)// 汉诺塔问题
+        {// from -> to           
+            if (num == 1)
             {
                 return 1;
             }
             else
             {
-                int 结果 = 0;
-                结果 += 2 * 汉诺塔(数量 - 1,柱子1, 柱子2, 柱子3) + 1;
-                return 结果;
+                int result = 0;
+                result += 2 * Hanoi(num - 1) + 1;
+                return result;
             }      
         }
     }
